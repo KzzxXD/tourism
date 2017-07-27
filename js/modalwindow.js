67b9017a333b201e9a89217498c691a6a8205aaa
Roman <romantylchak29@gmail.com>
2017-07-27 13:19:18 +0000

    adapt

--- js/modalwindow.js
+++ js/modalwindow.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
     $('a.modal-window-moreinfo').click(function (event) {
         event.preventDefault();
-        $('#pillow').fadeIn(400,
+        $('#pillow').slideDown(600,
             function () {
             $('#modal-window')
                 .css('display', 'block')
@@ -13,14 +13,14 @@ $(document).ready(function () {
            .animate({opacity: 0, top: '45%'},200,
            function () {
               $(this).css('display', 'none');
-              $('#pillow').fadeOut(400);
+              $('#pillow').slideUp(600);
            });
     });
 });
 $(document).ready(function () {
    $('a.modal-window-moreinfo-vancouver').click(function (event) {
       event.preventDefault();
-      $('#pillow-vancouver').fadeIn(400,
+      $('#pillow-vancouver').slideDown(400,
       function () {
          $('#modal-window-vancouver')
              .css('display', 'block')
@@ -32,14 +32,14 @@ $(document).ready(function () {
           .animate({opacity: 0, top: '45%'},200,
           function () {
              $(this).css('display', 'none');
-             $('#pillow-vancouver').fadeOut(400);
+             $('#pillow-vancouver').slideUp(600);
           });
    });
 });
 $(document).ready(function () {
     $('a.modal-window-moreinfo-barcelona').click(function (event) {
         event.preventDefault();
-        $('#pillow-barcelona').fadeIn(400,
+        $('#pillow-barcelona').slideDown(400,
             function () {
                 $('#modal-window-barcelona')
                     .css('display', 'block')
@@ -51,7 +51,64 @@ $(document).ready(function () {
             .animate({opacity: 0, top: '45%'},200,
                 function () {
                     $(this).css('display', 'none');
-                    $('#pillow-barcelona').fadeOut(400);
+                    $('#pillow-barcelona').slideUp(600);
+                });
+    });
+});
+$(document).ready(function () {
+    $('a.modal-window-moreinfo-wicklow').click(function (event) {
+       event.preventDefault();
+       $('#pillow-wicklow').slideDown(400,
+       function () {
+          $('#modal-window-wicklow')
+              .css('display', 'block')
+              .animate({opacity: 1, top: '50%'},200);
+       });
+    });
+   $('#modal-window-close-wicklow, #pillow-wicklow').click(function () {
+        $('#modal-window-wicklow')
+            .animate({opacity: 0, top: '45%'},200,
+            function () {
+                $(this).css('display', 'none');
+                $('#pillow-wicklow').slideUp(600);
+            });
+   });
+});
+$(document).ready(function () {
+    $('a.modal-window-moreinfo-bruges').click(function (event) {
+        event.preventDefault();
+        $('#pillow-bruges').slideDown(400,
+            function () {
+                $('#modal-window-bruges')
+                    .css('display', 'block')
+                    .animate({opacity: 1, top: '50%'},200);
+            });
+    });
+    $('#modal-window-close-bruges, #pillow-bruges').click(function () {
+        $('#modal-window-bruges')
+            .animate({opacity: 0, top: '45%'},200,
+                function () {
+                    $(this).css('display', 'none');
+                    $('#pillow-bruges').slideUp(600);
+                });
+    });
+});
+$(document).ready(function () {
+    $('a.modal-window-moreinfo-prague').click(function (event) {
+        event.preventDefault();
+        $('#pillow-prague').slideDown(400,
+            function () {
+                $('#modal-window-prague')
+                    .css('display', 'block')
+                    .animate({opacity: 1, top: '50%'},200);
+            });
+    });
+    $('#modal-window-close-prague, #pillow-prague').click(function () {
+        $('#modal-window-prague')
+            .animate({opacity: 0, top: '45%'},200,
+                function () {
+                    $(this).css('display', 'none');
+                    $('#pillow-prague').slideUp(600);
                 });
     });
 });
